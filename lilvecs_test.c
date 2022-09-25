a15f12977feabc791d2c2c87d7b5fbb70add8053
nondefault <spam@nondefault.net>
2022-09-25 14:00:21 -0700

    batshit generic thing

--- lilvecs_test.c
+++ lilvecs_test.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
 #include "lilvecs.h"
+#include <stdio.h>
 
-void print_int(void* item, uint32_t i) {
-  printf("index %d: %d\n", i, *((int*)item));
+VEC_INSTANCE(int);
+
+void print_int(int* item, uint32_t i) {
+  printf("index %d: %d\n", i, *item);
 }
 
-int main(int argc, char const *argv[]) {
-  Vec v;
-  vec_create(&v, sizeof(int));
+int main(int argc, char const* argv[]) {
+  Vec_int v;
+  vec_create_int(&v);
 
   for (int i = 0; i < 100; ++i) {
-    vec_push(&v, &i);
+    vec_push_int(&v, &i);
   }
 
-  vec_foreach(&v, print_int);
+  vec_foreach_int(&v, print_int);
   printf("vec capacity: %d\n", v.capacity);
 
   return 0;
