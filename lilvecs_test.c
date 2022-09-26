86f0355f856577e99307688745ed61fcbdacd81c
nondefault <spam@nondefault.net>
2022-09-26 02:43:47 -0700

    add insert and remove

--- lilvecs_test.c
+++ lilvecs_test.c
@@ -11,6 +11,16 @@ typedef struct Point {
 VEC_DECL(int)
 VEC_DECL(Point)
 
+void print_vec_int(Vec_int* v) {
+  for (int i = 0; i < vec_size_int(v); ++i) {
+    if (i > 0) {
+      printf(", ");
+    }
+    printf("%d", vec_get_int(v, i));
+  }
+  printf("\n");
+}
+
 void test_create() {
   Vec_int* vi = vec_create_int();
   assert(vi);
@@ -106,6 +116,118 @@ void test_cleanup() {
   }
 }
 
+void test_insert() {
+  // middle
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+    vec_push_int(v, 20);
+    vec_push_int(v, 30);
+
+    vec_insert_int(v, 1, 15);
+    assert(vec_size_int(v) == 4);
+    assert(vec_get_int(v, 0) == 10);
+    assert(vec_get_int(v, 1) == 15);
+    assert(vec_get_int(v, 2) == 20);
+    assert(vec_get_int(v, 3) == 30);
+    vec_delete_int(v);
+  }
+  // beginning
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+    vec_push_int(v, 20);
+    vec_push_int(v, 30);
+
+    vec_insert_int(v, 0, 5);
+    assert(vec_size_int(v) == 4);
+    assert(vec_get_int(v, 0) == 5);
+    assert(vec_get_int(v, 1) == 10);
+    assert(vec_get_int(v, 2) == 20);
+    assert(vec_get_int(v, 3) == 30);
+    vec_delete_int(v);
+  }
+  // end
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+    vec_push_int(v, 20);
+    vec_push_int(v, 30);
+
+    vec_insert_int(v, 3, 35);
+    assert(vec_size_int(v) == 4);
+    assert(vec_get_int(v, 0) == 10);
+    assert(vec_get_int(v, 1) == 20);
+    assert(vec_get_int(v, 2) == 30);
+    assert(vec_get_int(v, 3) == 35);
+    vec_delete_int(v);
+  }
+  // empty
+  {
+    Vec_int* v = vec_create_int();
+
+    vec_insert_int(v, 0, 10);
+    assert(vec_size_int(v) == 1);
+    assert(vec_get_int(v, 0) == 10);
+    vec_delete_int(v);
+  }
+}
+
+void test_remove() {
+  // middle
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+    vec_push_int(v, 20);
+    vec_push_int(v, 30);
+
+    int removed = vec_remove_int(v, 1);
+    assert(removed == 20);
+    assert(vec_size_int(v) == 2);
+    assert(vec_get_int(v, 0) == 10);
+    assert(vec_get_int(v, 1) == 30);
+    vec_delete_int(v);
+  }
+  // beginning
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+    vec_push_int(v, 20);
+    vec_push_int(v, 30);
+
+    int removed = vec_remove_int(v, 0);
+    assert(removed == 10);
+    assert(vec_size_int(v) == 2);
+    assert(vec_get_int(v, 0) == 20);
+    assert(vec_get_int(v, 1) == 30);
+    vec_delete_int(v);
+  }
+  // end
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+    vec_push_int(v, 20);
+    vec_push_int(v, 30);
+
+    int removed = vec_remove_int(v, 2);
+    assert(removed == 30);
+    assert(vec_size_int(v) == 2);
+    assert(vec_get_int(v, 0) == 10);
+    assert(vec_get_int(v, 1) == 20);
+    vec_delete_int(v);
+  }
+  // only item
+  {
+    Vec_int* v = vec_create_int();
+    vec_push_int(v, 10);
+
+    int removed = vec_remove_int(v, 0);
+    assert(removed == 10);
+    assert(vec_size_int(v) == 0);
+    vec_delete_int(v);
+  }
+}
+
 int main(int argc, char const* argv[]) {
   printf("test_create\n");
   test_create();
@@ -121,6 +243,10 @@ int main(int argc, char const* argv[]) {
   test_set();
   printf("test_cleanup\n");
   test_cleanup();
+  printf("test_insert\n");
+  test_insert();
+  printf("test_remove\n");
+  test_remove();
 
   return 0;
 }
