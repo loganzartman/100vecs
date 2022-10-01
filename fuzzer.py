4647124016dd09c6a59b5aee0e7c9e35acd67c08
Logan <logan.zartman@utexas.edu>
2022-10-01 14:44:41 -0700

    fuzzer: debug and don't leak

--- fuzzer.py
+++ fuzzer.py
@@ -3,8 +3,6 @@
 from random import choice, randint
 import subprocess
 
-from requests import delete
-
 
 def runner(body):
   return f"""
@@ -34,17 +32,28 @@ def c_literal(x):
   raise Exception(f"Unsupported literal: {type(x)}")
 
 
+def un_literal(x):
+  if isinstance(x, str):
+    return x.lstrip('"').rstrip('"')
+  return str(x)
+
+
 def run_code(code):
   with open("fuzzer_gen.c", "w+") as f:
     f.write(code)
-  subprocess.run(["make", "fuzzer_gen"], check=True)
+  subprocess.run(
+      "CFLAGS='-fsanitize=address -fno-omit-frame-pointer' make fuzzer_gen",
+      shell=True,
+      check=True
+  )
   result_bytes = subprocess.check_output(["./fuzzer_gen"])
   result = result_bytes.decode('utf-8')
   return result.splitlines()
 
 
 class MapSimulator:
-  def __init__(self):
+  def __init__(self, debug_faults):
+    self.debug_faults = debug_faults
     self.type_k = "int"
     self.type_v = "int"
     self.map_id = 0
@@ -59,9 +68,15 @@ class MapSimulator:
     return f"map_{op}_{self.type_k}__{self.type_v}({', '.join(args)})"
 
   def map_op(self, op, *args):
+    if self.debug_faults:
+      self.statements.append(
+          f'printf("{op} {" ".join(map(un_literal, args))}\\n");\nmap_debug_int__int(m{self.map_id});')
     return self.map_raw_op(op, f"m{self.map_id}", *args)
 
   def create(self):
+    self.map_id += 1
+    if self.map_id > 1:
+      self.statements.append(self.map_op("destroy") + ";")
     decl = f"{self.map_type()}* m{self.map_id}"
     op = self.map_raw_op("create", "hash_int", "eq_int")
     self.statements.append(f"{decl} = {op};")
@@ -82,18 +97,23 @@ class MapSimulator:
     self.expectations.append(self.model[k])
 
   def render(self):
-    return runner('\n'.join(self.statements))
+    return runner('\n'.join(self.statements + [self.map_op("destroy") + ";"]))
 
   def run_and_assert(self):
     results = run_code(self.render())
-    for i, (actual, expected) in enumerate(zip(results, self.expectations)):
-      if actual != str(expected):
-        raise AssertionError(
-            f"Checkpoint {i}: expected: {str(expected)}, actual: {actual}")
+    if self.debug_faults:
+      print("Debug mode; showing output:")
+      for line in results:
+        print(line)
+    else:
+      for i, (actual, expected) in enumerate(zip(results, self.expectations)):
+        if actual != str(expected):
+          raise AssertionError(
+              f"Checkpoint {i}: expected: {str(expected)}, actual: {actual}")
 
 
 def main():
-  map_sim = MapSimulator()
+  map_sim = MapSimulator(debug_faults=False)
   map_sim.create()
   for _ in range(1000):
     r = randint(0, 2)
